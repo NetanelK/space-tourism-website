13ecc82991fb2d17f2005af302937a934850a160
NetanelK <kadoshnetanel@gmail.com>
2021-12-03 14:50:23 +0200

    crew loop fix

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -8,23 +8,24 @@ const crewNames = crew.map((crewman) =>
     crewman.name.split(" ")[0].toLowerCase()
 );
 const loopCrew = crewNames.map((crew) => crew);
+loopCrew.push(loopCrew.shift());
 
 const Crew = (props) => {
     const [activeCrew, setActiveCrew] = useState("douglas");
 
+    useEffect(() => {
+        let selectedCrew = loopCrew.shift();
+        console.log("hi");
+        loopCrew.push(selectedCrew);
+        setTimeout(() => {
+            console.log(selectedCrew, loopCrew);
+            setActiveCrew(selectedCrew);
+        }, 5000);
+    }, [activeCrew]);
+
     const activeCrewData = crew.filter(
         (crewman) => crewman.name.toLowerCase().includes(activeCrew) && crewman
     )[0];
-
-    useEffect(() => {
-        let selectedCrew;
-        setTimeout(() => {
-            selectedCrew = loopCrew.shift();
-            loopCrew.push(selectedCrew);
-            console.log(selectedCrew, loopCrew, crewNames);
-            setActiveCrew(selectedCrew);
-        }, 5000);
-    });
 
     const handleClick = (crew) => {
         setActiveCrew(crew);
